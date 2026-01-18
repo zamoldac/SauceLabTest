395a0498706df275ff72a3c966215d1420771901
Alex <alexandru.ghita@uipath.com>
2026-01-18 15:48:11 +0200

    adding test data json

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -6,16 +6,19 @@ export class LoginPage {
   readonly pageTitle: Locator;
   readonly userNameField: Locator;
   readonly passwordField: Locator;
+  readonly loginError: Locator;
   readonly productPage: Locator;
+
 
 
   constructor(page: Page) {
     this.page = page;
-    this.pageTitle = page.getByText("Swag Labs");
+    this.pageTitle = page.getByText("Swag Labsa");
     this.userNameField = page.locator("#user-name");
     this.passwordField = page.locator("#password");
     this.loginBtn = page.locator("#login-button");
-    this.productPage = page.getByText("Products")
+    this.productPage = page.getByText("Products");
+    this.loginError = page.locator("[data-test='error']");
   }
 
   async goToLoginPage() {
@@ -46,13 +49,7 @@ export class LoginPage {
     await expect(this.productPage).not.toBeVisible();  
     }
 
-  // async getStarted() {
-  //   await this.getStartedLink.first().click();
-  //   await expect(this.gettingStartedHeader).toBeVisible();
-  // }
-
-  // async pageObjectModel() {
-  //   await this.getStarted();
-  //   await this.pomLink.click();
-  // }
+    async assertLoginError(error:string){
+    await expect (this.loginError).toHaveText(error);
+    }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,28 +1,66 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import * as testData from '../testData/testData.json';
 
-test('verify login failed', async ({ page }) => {
+test('this test verifies that invalid credentials lead to unsuccessful login', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goToLoginPage();
-
-  // Expect a title "to contain" a substring.
   await expect(loginPage.verifyTitle).toBeTruthy();
-  await loginPage.fillUsername("test");
-  await loginPage.fillPassword("pass");
+  await loginPage.fillUsername(testData.invalidUser.username);
+  await loginPage.fillPassword(testData.invalidUser.password);
   await loginPage.clickLogin();
-  await loginPage.assertLoginFailed();
+  await loginPage.assertLoginError(testData.invalidUser.expectedError);
 
 });
 
-test('verify login successful', async ({ page }) => {
+test('this test verifies that valid credentials lead to successful login', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goToLoginPage();
-
-  // Expect a title "to contain" a substring.
   await expect(loginPage.verifyTitle).toBeTruthy();
   await loginPage.fillUsername("standard_user");
   await loginPage.fillPassword("secret_sauce");
   await loginPage.clickLogin();
   await loginPage.loginSuccessful();
+
+});
+
+test('this test verifies login page behavior for empty username field login attempt', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goToLoginPage();
+  await expect(loginPage.verifyTitle).toBeTruthy();
+  await loginPage.fillPassword("secret_sauce");
+  await loginPage.clickLogin();
+  await loginPage.assertLoginError("Epic sadface: Username is required");
+});
+
+test('this test verifies login page behavior for empty password field login attempt', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goToLoginPage();
+  await expect(loginPage.verifyTitle).toBeTruthy();
+  await loginPage.fillUsername("standard_user");
+  await loginPage.clickLogin();
+  await loginPage.assertLoginError("Epic sadface: Password is required");
+
+});
+
+test('this test verifies login page behavior for locked out user credentials login attempt', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goToLoginPage();
+  await expect(loginPage.verifyTitle).toBeTruthy();
+  await loginPage.fillUsername("locked_out_user");
+  await loginPage.fillPassword("secret_sauce");
+  await loginPage.clickLogin();
+  await loginPage.assertLoginError("Epic sadface: Sorry, this user has been locked out.");
+
+});
+
+test('this test verifies login page behavior for missmatch password login attempt', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goToLoginPage();
+  await expect(loginPage.verifyTitle).toBeTruthy();
+  await loginPage.fillUsername("standard_user");
+  await loginPage.fillPassword("xyz");
+  await loginPage.clickLogin();
+  await loginPage.assertLoginError("Epic sadface: Username and password do not match any user in this service");
 
 });
