e5d4b820e94696e4d31dbd2770a1c65664840789
Alex <alexandru.ghita@uipath.com>
2026-01-19 16:18:00 +0200

    removed unused stuff from storePage

--- pages/StorePage.ts
+++ pages/StorePage.ts
@@ -6,18 +6,6 @@ export class StorePage {
   readonly hamburgerMenuBtn: Locator;
   readonly closeHamburgerMenuBtn: Locator;
   readonly logoutBtn: Locator;
-  readonly addToCartBtnBackPack: Locator;
-  readonly addToCartBtnBikeLight: Locator;
-  readonly addToCartBtnTShirt: Locator;
-  readonly addToCartBtnFJacket: Locator;
-  readonly addToCartBtnOnsie: Locator;
-  readonly addToCartBtnRedShirt: Locator;
-  readonly remToCartBtnBackPack: Locator;
-  readonly remToCartBtnBikeLight: Locator;
-  readonly remToCartBtnTShirt: Locator;
-  readonly remToCartBtnFJacket: Locator;
-  readonly remToCartBtnOnsie: Locator;
-  readonly remToCartBtnRedShirt: Locator;
   readonly cartBtn: Locator;
   readonly cartItemCounter: Locator;
   readonly hamMenuAllItems: Locator;
@@ -52,18 +40,6 @@ export class StorePage {
     this.hamburgerMenuBtn = page.locator("#react-burger-menu-btn");
     this.closeHamburgerMenuBtn = page.locator("#react-burger-cross-btn")
     this.logoutBtn = page.locator("#logout_sidebar_link");
-    this.addToCartBtnBackPack = page.locator("#add-to-cart-sauce-labs-backpack");
-    this.addToCartBtnBikeLight = page.locator("#add-to-cart-sauce-labs-bike-light");
-    this.addToCartBtnTShirt = page.locator("#add-to-cart-sauce-labs-bolt-t-shirt");
-    this.addToCartBtnFJacket = page.locator("#add-to-cart-sauce-labs-fleece-jacket");
-    this.addToCartBtnOnsie = page.locator("#add-to-cart-sauce-labs-onesie");
-    this.addToCartBtnRedShirt = page.locator("[id='add-to-cart-test.allthethings()-t-shirt-(red)']");
-    this.remToCartBtnBackPack = page.locator("#remove-sauce-labs-backpack");
-    this.remToCartBtnBikeLight = page.locator("#remove-sauce-labs-bike-light");
-    this.remToCartBtnTShirt = page.locator("#remove-sauce-labs-bolt-t-shirt");
-    this.remToCartBtnFJacket = page.locator("#remove-sauce-labs-fleece-jacket");
-    this.remToCartBtnOnsie = page.locator("#remove-sauce-labs-onesie");
-    this.remToCartBtnRedShirt = page.locator("[id='remove-test.allthethings()-t-shirt-(red)']");
     this.cartBtn = page.locator("#shopping_cart_container");
     this.cartItemCounter = page.locator("[data-test='shopping-cart-badge']");
     this.hamMenuAllItems = page.locator("#inventory_sidebar_link");
